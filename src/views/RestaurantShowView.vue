481df6f06e4250527b94aaa7386d1c2f81ddda78
SmolIndieGame <smolindiegame@gmail.com>
2024-09-14 15:39:58 +0800

    make changes

---
+++ src/views/RestaurantShowView.vue
@@ -0,0 +1,23 @@
+<script>
+import { restaurants } from "@/utils/store";
+import { computed, watch } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const restaurant = computed(() => restaurants.value);
+const load = () => {
+  restaurant.value;
+};
+
+watch(
+  () => route.params,
+  () => load(),
+  { immediate: true, deep: true }
+);
+</script>
+
+<template>
+  <h1></h1>
+</template>
+
+<style scoped></style>
