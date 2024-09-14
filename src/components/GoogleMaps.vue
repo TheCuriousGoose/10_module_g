035374cd2691759b4573e86a21064ab191999cbf
Justin <ws25@wordskills.org>
2024-09-14 09:47:14 +0200

    Modified GoogleMaps to use the store

--- src/components/GoogleMaps.vue
+++ src/components/GoogleMaps.vue
@@ -1,23 +1,8 @@
 <script setup>
 import { onMounted } from 'vue';
+import { restaurants } from '@/utils/store'
 
-const markerLocations = [
-    { lat: 45.7597, lng: 4.8426 },
-    { lat: 45.7580, lng: 4.8357 },
-    { lat: 45.7582, lng: 4.8325 },
-    { lat: 45.7579, lng: 4.8314 },
-    { lat: 45.7622, lng: 4.8415 },
-    { lat: 45.7590, lng: 4.8422 },
-    { lat: 45.7640, lng: 4.8357 },
-    { lat: 45.7593, lng: 4.8326 },
-    { lat: 45.7608, lng: 4.8321 },
-    { lat: 45.7567, lng: 4.8387 },
-    { lat: 45.7615, lng: 4.8421 },
-    { lat: 45.7589, lng: 4.8343 },
-    { lat: 45.7600, lng: 4.8373 },
-    { lat: 45.7637, lng: 4.8331 },
-    { lat: 45.7570, lng: 4.8310 }
-]
+const restaurantsList = restaurants;
 
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
@@ -32,10 +17,14 @@ async function initMap() {
         mapId: 'default',
     });
 
-    markerLocations.forEach((location) => {
+    restaurantsList.value.forEach((restuarant) => {
         const marker = new AdvancedMarkerElement({
             map: map,
-            position: location
+            position: {
+                lat: restuarant.latitude,
+                lng: restuarant.longitude
+            },
+            title: restuarant.name
         })
     })
 }
